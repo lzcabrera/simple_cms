30e208354aada82f5d5510cab4e7679607c7e61f
Laura Cabrera <lzcabrera@gmail.com>
2011-07-06 08:11:58 -0700

    just before starting chp6 exercise

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,15 @@
 class DemoController < ApplicationController
   def index
+    # render('hello')
+    redirect_to(:action => 'other_hello')
+  end
+  
+  def hello
+    redirect_to('http://www.funnelcity.com')
+  end
+  
+  def other_hello
+    render(:text => 'Hello Everyone')
   end
 
 end
