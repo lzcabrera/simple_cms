9e53743eaa5c105c0e3493fbd25b6e4b3d3676a2
Laura Cabrera <lzcabrera@gmail.com>
2011-08-11 19:21:17 -0700

    up until the end of 9.3

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,7 +1,20 @@
 class SectionsController < ApplicationController
   
+  def index
+    list
+    render('list')
+  end
+  
   def list
     @sections = Section.order("sections.position ASC")
   end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+  
+  
+  
+  
   
 end
