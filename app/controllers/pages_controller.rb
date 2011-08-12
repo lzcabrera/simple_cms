d77c48706368cbc6f7dbea49ae13c716945a7f40
Laura Cabrera <lzcabrera@gmail.com>
2011-08-11 21:31:04 -0700

    9.3 shows

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
   
+  def index
+    list
+    render('list')
+  end
+  
   def list
     @pages = Page.order("pages.position ASC")
+  end
+  
+  def show
+    @page = Page.find(params[:id])
   end
   
 end
