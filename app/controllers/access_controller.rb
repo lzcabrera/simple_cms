79ab9beb73bdf90701c02410a6cb774a0e34c548
Laura Cabrera <lzcabrera@gmail.com>
2011-08-20 23:54:11 -0700

    completed chapter 13 - user authentication

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,9 @@ class AccessController < ApplicationController
   
   layout 'admin'
   
+  before_filter :confirm_logged_in, :except => [:login, :attempt_login, :logout]
+
+  
   def index
     menu
     render('menu')
